ec984e42996224ddf90a486dfe7bca0ccd0567a7
webWorker-eos <1574533602@qq.com>
2020-09-03 13:54:24 +0800

    Versions

--- ui/apps2/src/views/konami/dialog/Version.vue
+++ ui/apps2/src/views/konami/dialog/Version.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="version">
     <div>Versions V2.0.32</div>
-    <div class="updata">{{ $t('public.lastUpdate') }}: 2020-09-03 00:50</div>
+    <div class="updata">{{ $t('public.lastUpdate') }}: 2020-09-03 14:00</div>
   </div>
 </template>
 
