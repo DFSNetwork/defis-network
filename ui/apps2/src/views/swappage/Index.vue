a955e4eb9019d174d3f239607982d6a20d4410b4
webWorker-eos <1574533602@qq.com>
2020-10-12 22:52:16 +0800

    rose coin

--- ui/apps2/src/views/swappage/Index.vue
+++ ui/apps2/src/views/swappage/Index.vue
@@ -509,7 +509,7 @@ export default {
     },
     handleDealRouteImg(item) {
       const localeCoin = ['eosio.token-eos', 'bankofusddv1-usdd', 'whaleextoken-wal'];
-      const localCoinPng = ['hbbguanfang5-hbb', 'cynthiacaoyi-cbed', 'huangheeos.e-jcb', 'buyniubinbbb-nbb',
+      const localCoinPng = ['hbbguanfang5-hbb', 'cynthiacaoyi-cbed', 'huangheeos.e-jcb', 'buyniubinbbb-nbb', 'rosedefifarm-rose',
       'yfctokenmain-yfc', 'eossanguotkt-tkt', 'pink.bank-pink', 'dbctokenmain-dbc', 'sars.run-eet', 'looptoken123-loop',
       'lootglobcore-loot', 'pddtokenmain-pdd']
       const inData = item.toLowerCase().replace(':', '-')
