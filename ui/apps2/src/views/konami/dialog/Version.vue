0e8b0616222bcaeccf34925ccd565e2f1af0a59e
webWorker-eos <1574533602@qq.com>
2020-10-17 01:45:05 +0800

    loot && time

--- ui/apps2/src/views/konami/dialog/Version.vue
+++ ui/apps2/src/views/konami/dialog/Version.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="version">
-    <div>Versions V3.0.03</div>
-    <div class="updata">{{ $t('public.lastUpdate') }}: 2020-10-16 20:20</div>
+    <div>Versions V3.0.04</div>
+    <div class="updata">{{ $t('public.lastUpdate') }}: 2020-10-17 01:45</div>
   </div>
 </template>
 
