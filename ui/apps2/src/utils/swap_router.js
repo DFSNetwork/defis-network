97d31637bca1a4e629f00ff650a650ba5382971f
webWorker-eos <1574533602@qq.com>
2020-10-22 00:23:05 +0800

    path length

--- ui/apps2/src/utils/swap_router.js
+++ ui/apps2/src/utils/swap_router.js
@@ -195,7 +195,7 @@ class swapRouter {
       }
       _pathsMids.push(mids + '') // 返回所有Mid路径
     })
-    console.log(_pathsMids)
+    // console.log(_pathsMids)
     // return [_pathsMids[0]];
     return _pathsMids;
   }
