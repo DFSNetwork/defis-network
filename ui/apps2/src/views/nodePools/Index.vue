c512b76d17e7be8da4964d56f44d2252f8496078
webWorker-eos <1574533602@qq.com>
2020-11-03 13:27:01 +0800

    node pools

---
+++ ui/apps2/src/views/nodePools/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'nodePools',
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
