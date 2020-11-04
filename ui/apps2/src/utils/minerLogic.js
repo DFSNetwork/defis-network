751f1e3ca7e678ee0f0efaab534b75a832241ef1
webWorker-eos <1574533602@qq.com>
2020-11-04 15:47:38 +0800

    time reward

--- ui/apps2/src/utils/minerLogic.js
+++ ui/apps2/src/utils/minerLogic.js
@@ -18,8 +18,6 @@ export function timeApy(market, type, pool) {
   } // || sT <= 0
   const price = parseFloat(pool.reserve0) / (parseFloat(pool.reserve1) || 1) // TIME 当前价格
   const eos = parseFloat(market.reserve0) || 1;
-  // const price = 0.001
-  // const eos = 10000
   try {
     if (type === 'year') {
       const times = Math.ceil(sT / li.halftime);
@@ -37,7 +35,6 @@ export function timeApy(market, type, pool) {
   return 0
 }
 
-
 export function timeNum(market, userLP, rankWeight) {
   const mid = market.mid;
   const timeList = store.state.config.timeList; // TIME LP挖矿列表
@@ -52,14 +49,14 @@ export function timeNum(market, userLP, rankWeight) {
     const lamp = Math.pow(2, times); // 衰减系数
     const tNum = parseFloat(li.max_supply) - parseFloat(li.supply); // 池子剩余数量
     const secGet = tNum / lamp / 2 / li.halftime;
-    const num = secGet * sT * (parseInt(userLP.liq_bal0) / market.liquidity_token) * rankWeight.constant;
+    const num = secGet * sT * (parseInt(userLP.liq_bal0) / market.liquidity_token / 2) * rankWeight.constant;
+    // console.log(num)
     return num
   } catch (error) {
     console.log(error)
   }
   return 0
 }
-
 
 export function timeDssNum(li, userLP, rankWeight, allStaked) {
   const nowTime = Date.parse(new Date()) / 1000;
@@ -74,7 +71,8 @@ export function timeDssNum(li, userLP, rankWeight, allStaked) {
     const lamp = Math.pow(2,times); // 衰减系数
     const tNum = parseFloat(li.max_supply) - parseFloat(li.supply); // 池子剩余数量
     const secGet = tNum / lamp / 2 / 604800;
-    const num = secGet * sT * (parseFloat(userLP.liq_bal0) / parseFloat(allStaked.stackasset)) * rankWeight.constant * uPool;
+    const num = secGet * sT * (parseFloat(userLP.liq_bal0) / parseFloat(allStaked.stackasset) / 2) * rankWeight.constant * uPool;
+    // console.log(num)
     return num
   } catch (error) {
     console.log(error)
