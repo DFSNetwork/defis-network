bcd51467827b3a0be6aac648125d3c6e367cfde4
webWorker-eos <1574533602@qq.com>
2020-11-20 22:10:20 +0800

    bug deal

--- ui/apps2/src/views/konami/dialog/Version.vue
+++ ui/apps2/src/views/konami/dialog/Version.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="version">
-    <div>Versions V3.0.27</div>
-    <div class="updata">{{ $t('public.lastUpdate') }}: 2020-11-20 17:00</div>
+    <div>Versions V3.0.28</div>
+    <div class="updata">{{ $t('public.lastUpdate') }}: 2020-11-20 22:10</div>
   </div>
 </template>
 
