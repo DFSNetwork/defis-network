3bcb5ae5cc254fdbecb77959a36a7c6694ef2112
webWorker-eos <1574533602@qq.com>
2020-11-28 16:01:53 +0800

    ogx swap

--- ui/apps2/src/views/konami/dialog/Version.vue
+++ ui/apps2/src/views/konami/dialog/Version.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="version">
-    <div>Versions V3.0.30</div>
-    <div class="updata">{{ $t('public.lastUpdate') }}: 2020-11-26 19:20</div>
+    <div>Versions V3.0.31</div>
+    <div class="updata">{{ $t('public.lastUpdate') }}: 2020-11-28 16:00</div>
   </div>
 </template>
 
