3429640b502ca7e2ab2e43e0f2a24eda37d579fb
webWorker-eos <1574533602@qq.com>
2021-01-18 16:11:43 +0800

    bet filter

--- ui/apps2/src/views/pddex/comp/appLogic.js
+++ ui/apps2/src/views/pddex/comp/appLogic.js
@@ -193,6 +193,12 @@ export function dealAreaArr(arr, coin) {
   const coinPrice = getAreaPrice(coin);
   arr.forEach(list => {
     let v = list;
+    if (v.contract0 === 'bgbgbgbgbgbg' || v.contract0 === 'betdicetoken' || v.contract0 === 'sportbetsbet'
+     || v.contract0 === 'betasharetkn' || v.contract0 === 'betacorecash' || v.contract0 === 'betkingtoken'
+     || v.contract1 === 'bgbgbgbgbgbg' || v.contract1 === 'betdicetoken' || v.contract1 === 'sportbetsbet'
+     || v.contract1 === 'betasharetkn' || v.contract1 === 'betacorecash' || v.contract1 === 'betkingtoken') {
+      return
+    }
     if (!regExchange(coin, v)) {
       const tLi = {
         contract0: v.contract1,
